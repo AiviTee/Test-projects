ffa075205ce9d025b558afa74b7af4e70dcd29a6
Anna <anna2.morozova.it@gmail.com>
2026-07-20 13:05:44 +0300

    Add files via upload

---
+++ app/models.py
@@ -0,0 +1,52 @@
+from sqlalchemy import Column, Integer, String, Boolean, Table, ForeignKey
+from sqlalchemy.orm import relationship, declarative_base
+
+Base = declarative_base()
+
+# Таблицы many-to-many
+job_location = Table(
+    "job_location",
+    Base.metadata,
+    Column("job_id", ForeignKey("jobs.id"), primary_key=True),
+    Column("location_id", ForeignKey("locations.id"), primary_key=True),
+)
+
+job_skill = Table(
+    "job_skill",
+    Base.metadata,
+    Column("job_id", ForeignKey("jobs.id"), primary_key=True),
+    Column("skill_id", ForeignKey("skills.id"), primary_key=True),
+)
+
+class Location(Base):
+    __tablename__ = "locations"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+
+    # У локации есть список вакансий -> свойство называется jobs
+    jobs = relationship("Job", secondary=job_location, back_populates="locations")
+
+
+class Skill(Base):
+    __tablename__ = "skills"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+
+    # У навыка есть список вакансий
+    jobs = relationship("Job", secondary=job_skill, back_populates="skills")
+
+
+class Job(Base):
+    __tablename__ = "jobs"
+
+    id = Column(Integer, primary_key=True)
+    title = Column(String, nullable=False)
+    description = Column(String)
+    salary = Column(Integer)
+    remote = Column(Boolean, default=False)
+
+    # У вакансии есть список локаций и навыков
+    locations = relationship("Location", secondary=job_location, back_populates="jobs")
+    skills = relationship("Skill", secondary=job_skill, back_populates="jobs")
